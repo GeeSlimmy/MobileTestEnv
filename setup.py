b34e2d4a59a80d24a2d1ba3fe02a17f0a226c2e1
GeeSlimmy <geefung@gmail.com>
2019-07-28 14:51:44 -0500

    updated version and name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pycoffee-library",
-    version='0.3',
+    name="robotmobile-library",
+    version='0.1',
     packages = find_packages('src'),
     package_dir = {'' : 'src'},
 )
